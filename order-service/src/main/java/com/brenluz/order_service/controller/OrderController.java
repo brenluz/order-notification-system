da70175408f328335e4229fe152f4ad6583d25e2
Breno <brenluz@outlook.com>
2026-04-29 18:04:00 -0300

    added swagger for documentation

--- order-service/src/main/java/com/brenluz/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/brenluz/order_service/controller/OrderController.java
@@ -4,6 +4,9 @@ import com.brenluz.order_service.dto.OrderRequest;
 import com.brenluz.order_service.dto.OrderResponse;
 import com.brenluz.order_service.service.OrderService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +17,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
+@Tag(name = "Orders", description = "Order management API")
 public class OrderController {
     private final OrderService orderService;
 
-
+    @Operation(summary = "Create a new order")
+    @ApiResponse(responseCode = "201", description = "Order created successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid request body")
     @Valid
     @PostMapping
     public ResponseEntity<OrderResponse> save(@RequestBody @Valid OrderRequest order) {
@@ -25,6 +31,9 @@ public class OrderController {
         return ResponseEntity.status(201).body(savedOrder);
     }
 
+
+    @Operation(summary = "Get all orders")
+    @ApiResponse(responseCode = "200", description = "Sucess")
     @GetMapping
     public ResponseEntity<List<OrderResponse>> findAll() {
         return ResponseEntity.ok(orderService.findAll());
