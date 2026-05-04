d68f13a40bdd1db5b430c673e5bb6e83f670a201
Breno <brenluz@outlook.com>
2026-05-03 21:01:06 -0300

    added dead letter queue for lost messages

--- notification-service/src/main/java/com/brenluz/notification_service/config/RabbitMQConfig.java
+++ notification-service/src/main/java/com/brenluz/notification_service/config/RabbitMQConfig.java
@@ -12,15 +12,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
     public static final String queue = "order.queue";
-
     public static final String exchange = "order.exchange";
-
     public static final String routingKey = "order.routingkey";
+
+    public static final String deadLetterQueueName = "order.dlq";
+    public static final String deadLetterExchange = "order.dlx";
+    public static final String deadLetterRoutingKey = "order.dlx.routingkey";
 
     @Bean
     public Queue orderQueue() {
-        return new Queue(queue);
+        return QueueBuilder.durable(queue)
+                .withArgument("x-dead-letter-exchange", deadLetterExchange)
+                .withArgument("x-dead-letter-routing-key", deadLetterRoutingKey)
+                .build();
     }
+
+    @Bean
+    public Queue deadLetterQueue() { return new Queue(deadLetterQueueName, true);}
 
     @Bean
     public TopicExchange orderExchange() {
@@ -28,11 +36,22 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    public TopicExchange deadLetterExchange() { return new TopicExchange(deadLetterExchange);}
+
+    @Bean
     public Binding orderBinding(Queue orderQueue, Exchange orderExchange) {
         return BindingBuilder
                 .bind(orderQueue)
                 .to((TopicExchange) orderExchange)
                 .with(routingKey);
+    }
+
+    @Bean
+    public Binding deadLetterBinding() {
+        return BindingBuilder
+                .bind(deadLetterQueue())
+                .to(deadLetterExchange())
+                .with(deadLetterRoutingKey);
     }
 
     @Bean
